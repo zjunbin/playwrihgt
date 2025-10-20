42ab773cb63c17319d265847e938abaa0ffab929
zjunbin <648260307@qq.com>
2025-10-20 12:48:10 +0800

    "message run.py file_path"

--- run.py
+++ run.py
@@ -4,9 +4,15 @@
 #@Author  :zjunbin
 #@File    :run.py
 """
+import os.path
+
 '''自动发现、收集、运行生成测试allure报告'''
 '查看测试报告   allure serve ./allure_report/'
 
 import pytest
-
-pytest.main(['-s', '-v', r'--alluredir=E:\djangoProject\playwright_test\allure_report', '--clean-alluredir'])
+path = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+print(path)
+allure_report= os.path.join(os.path.join(path,'playwright_test'),'allure_report')
+print(allure_report)
+pytest.main(['-s', '-v', r'--alluredir={}'.format(allure_report), '--clean-alluredir'])
+# pytest.main(['-s', '-v', r'--alluredir=E:\djangoProject\playwright_test\allure_report', '--clean-alluredir'])
